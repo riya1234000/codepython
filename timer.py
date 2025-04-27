9b1e873be42b2720d07858d84ba5397804c94a46
riya1234000 <tuliriya027@gmail.com>
2025-04-27 19:42:26 +0530

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,7 @@
+# import time
+# from plyer import notification
+# while True:
+#     print('please have a sip of water....!')
+#     notification.notify(title= "please drink some water", message= 'you need to drink some water')
+
+#     time.sleep(60*60)
