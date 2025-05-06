eda2f712cf7918f403429bb12ee88b1e13f295ca
riya1234000 <tuliriya027@gmail.com>
2025-05-06 16:10:33 +0530

    Update millionare_game.py

--- millionare_game.py
+++ millionare_game.py
@@ -6,34 +6,36 @@
 * if it is true them this loop will continue otherwise this loop will be break
 
 # """
+# 1 : question.....!
 
-# questions= [['What is the capital of France?', 'Paris', 'London', 'Berlin', 'Madrid', 1],
-# ['What is the largest planet in our solar system?', 'Earth', 'Mars', 'Jupiter', 'Saturn', 3],
-# ['What is the chemical symbol for gold?', 'Au', 'Ag', 'Fe', 'Pb', 1],
-# ['What is the smallest prime number?', '1', '2', '3', '4', 2],
+questions= [['What is the capital of France?', 'Paris', 'London', 'Berlin', 'Madrid', 1],
+            ['What is the largest planet in our solar system?', 'Earth', 'Mars', 'Jupiter', 'Saturn', 3],
+            ['What is the chemical symbol for gold?', 'Au', 'Ag', 'Fe', 'Pb', 1],
+            ['What is the smallest prime number?', '1', '2', '3', '4', 2],
 
-# ]
+ ]
 
-# prize= [10000,200000, 300000, 500000 ]
-# i=0
-# for question in questions:
+ prize= [10000,200000, 300000, 500000 ]
+ i=0
+ for question in questions:
     
-#     print(question[0])
-#     print(f'a.{question[1]}')
-#     print(f'b .{question[2]}')
-#     print(f' c.{question[3]}')
-#     print(f' d.{question[4]}')
+    print(question[0])
+     print(f'a.{question[1]}')
+     print(f'b .{question[2]}')
+     print(f' c.{question[3]}')
+     print(f' d.{question[4]}')
 
-#     user= int(input("enter the input from the user: 1 for a, 2 for b, 3 for c, 4 for d"))
-#     if user==question[5]:
-#         print(f'coreect answer.........!')
-#         print(f' proze is: {prize[i]}')
-#     else:
-#         print(f' wrong answer try again next time.........!')
-#         break
-#     print(f'you won........!{prize[i]}')
-#     i+=1
+     user= int(input("enter the input from the user: 1 for a, 2 for b, 3 for c, 4 for d"))
+     if user==question[5]:
+         print(f'coreect answer.........!')
+         print(f' proze is: {prize[i]}')
+     else:
+         print(f' wrong answer try again next time.........!')
+         break
+     print(f'you won........!{prize[i]}')
+     i+=1
 
+# 2 : question....!
 
 questions= [['what is the name of the person? ', 'riya', 'raghav', 'sharma', 'tuli', 2  ],
             ['what is capital of the farance? ', "paris", 'farance', 'delhi', 'mumbai', 1 ],
